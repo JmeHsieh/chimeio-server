a3b7bba58fd5f8fe17eb9fc8c67e0eb4bad684da
JmeHsieh <jmehsieh@gmail.com>
2016-04-18 11:56:40 +0800

    Filter out non-room-member for all service events - Message.

--- src/services/message/index.js
+++ src/services/message/index.js
@@ -17,13 +17,33 @@ module.exports = function() {
 
   // Initialize our service with any options it requires
   app.use('/messages', service(options));
+  const roomService = app.service('/rooms');
 
   // Get our initialize service to that we can bind hooks
   const messageService = app.service('/messages');
 
-  // Set up our before hooks
+  // Setup hooks
   messageService.before(hooks.before);
-
-  // Set up our after hooks
   messageService.after(hooks.after);
+
+  // Setup filters (here we will filter all events)
+  messageService.filter((data, connection) => {
+    const userIdField = '_id';
+
+    // filter out unauthenticated socket
+    const userId = connection.user[userIdField];
+    if (!userId) { return false; }
+
+    // filter out message sender him/her-self
+    if (userId.toString() === data.user.toString()) { return false; }
+
+    // filter out non-room-member socket
+    const roomId = data.room;
+    return new Promise((resolve, reject) => {
+      roomService.get(roomId, {}).then((room) => {
+        if (room.users.indexOf(userId) === -1) { reject(); }
+        resolve(data);
+      }).catch(reject);
+    });
+  });
 };
