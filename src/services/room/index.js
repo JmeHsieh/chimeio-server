fdd46557e815d3b0d7bd72027269094ce0adc349
JmeHsieh <jmehsieh@gmail.com>
2016-04-18 11:55:47 +0800

    Filter out all service events except for "created" - Room.

--- src/services/room/index.js
+++ src/services/room/index.js
@@ -17,13 +17,29 @@ module.exports = function() {
 
   // Initialize our service with any options it requires
   app.use('/rooms', service(options));
-
-  // Get our initialize service to that we can bind hooks
   const roomService = app.service('/rooms');
 
   // Set up our before hooks
   roomService.before(hooks.before);
-
-  // Set up our after hooks
   roomService.after(hooks.after);
+
+  // Setup filters (here we will filter all events)
+  roomService.filter({
+    created(data, connection) {
+      const userIdField = '_id';
+
+      // filter out unauthenticated socket
+      let userId = connection.user[userIdField];
+      if (!userId) { return false; }
+
+      // filter room creater him/her-self
+      if (userId.toString() === data.user.toString()) { return false; }
+
+      // filter out non-room-member socket
+      return data.users.indexOf(userId) !== -1;
+    },
+    updated() { return false; },
+    patched() { return false; },
+    removed() { return false; }
+  });
 };
