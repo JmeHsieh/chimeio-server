fd529853c7d1b2fd03e7ab5ecc8cbeca94472cee
JmeHsieh <jmehsieh@gmail.com>
2016-05-08 16:40:12 +0800

    Make User.username field not required.

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -11,7 +11,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 
   // attributes
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minLength: 8 },
   createdAt: { type: Date, 'default': Date.now },
